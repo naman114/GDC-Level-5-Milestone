9c107ecbf9ea9f3a381ffa4cea992b78f1f8c954
Naman Gogia <namangogia2001@gmail.com>
2022-01-31 21:09:57 +0530

    Adding level 5 milestone: tests passing

--- tasks/views.py
+++ tasks/views.py
@@ -8,7 +8,7 @@ completed_tasks = []
 
 # Pending tasks
 def tasks_view(request):
-    active_tasks = Task.objects.filter(deleted=False)
+    active_tasks = Task.objects.filter(deleted=False, completed=False)
     search_term = request.GET.get("search")
     if search_term:
         active_tasks = active_tasks.filter(title__icontains=search_term)
@@ -17,11 +17,24 @@ def tasks_view(request):
 
 # Completed tasks
 def completed_view(request):
+    completed_tasks = Task.objects.filter(completed=True)
+    search_term = request.GET.get("search")
+    if search_term:
+        completed_tasks = completed_tasks.filter(title__icontains=search_term)
     return render(request, "completed_tasks.html", {"tasks": completed_tasks})
 
 
 # All tasks
 def all_tasks_view(request):
+    active_tasks = Task.objects.filter(deleted=False, completed=False)
+    search_term = request.GET.get("search")
+    if search_term:
+        active_tasks = active_tasks.filter(title__icontains=search_term)
+
+    completed_tasks = Task.objects.filter(completed=True)
+    search_term = request.GET.get("search")
+    if search_term:
+        completed_tasks = completed_tasks.filter(title__icontains=search_term)
     return render(
         request,
         "all_tasks.html",
@@ -32,7 +45,6 @@ def all_tasks_view(request):
 # Add a task
 def add_task_view(request):
     task_to_add = request.GET.get("task")
-    # Instead of appending to the list, we save our task to the db
     Task(title=task_to_add).save()
     return HttpResponseRedirect("/tasks")
 
@@ -45,7 +57,5 @@ def delete_task_view(request, index):
 
 # Mark task as complete
 def complete_task_view(request, index):
-    if index <= len(active_tasks):
-        completed_tasks.append(active_tasks[index - 1])
-        del active_tasks[index - 1]
+    Task.objects.filter(id=index).update(completed=True)
     return HttpResponseRedirect("/tasks")
